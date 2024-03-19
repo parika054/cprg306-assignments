a6a339442ac8c93d64faa3666dc1d0dda056d2cd
parika054 <131924696+parika054@users.noreply.github.com>
2024-03-18 22:25:25 -0600

    Update page.js

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -18,11 +18,11 @@ function LandingPage() {
             <div className="flex justify-center items-center h-screen bg-blue-200">
                 <div className="text-center bg-black p-8 rounded-lg shadow-lg max-w-sm">
                     <p className="mb-4 text-lg">Authorization went Successful !!! <span className="font-semibold">{user.displayName}</span> ({user.email})</p>
-                    <button onClick={handleLogout} className="bg-blue-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    <button onClick={handleLogout} className="bg-blue-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         Logout
                     </button>
                     <div className="mt-4">
-                        <a href="/week-8/shopping-list" className="text-blue-500 hover:text-red-500">Click here to Go to Shopping List </a>
+                        <a href="/week-8/shopping-list" className="text-blue-400 hover:text-purple-500">Click here to Go to Shopping List </a>
                     </div>
                 </div>
             </div>
@@ -31,7 +31,7 @@ function LandingPage() {
         return (
             <div className="flex justify-center items-center h-screen bg-blue-100">
                 <div className="text-center p-8 ">
-                    <button onClick={handleLogin} className="bg-blue-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    <button onClick={handleLogin} className="bg-blue-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         Login with GitHub
                     </button>
                 </div>
