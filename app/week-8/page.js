51ce8848d927bfe03a1e01b85bc3ee7842abbe98
parika054 <131924696+parika054@users.noreply.github.com>
2024-04-10 15:40:33 -0600

    week8
    week8

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -16,13 +16,13 @@ function LandingPage() {
     if (user) {
         return (
             <div className="flex justify-center items-center h-screen bg-blue-200">
-                <div className="text-center bg-black p-8 rounded-lg shadow-lg max-w-sm">
-                    <p className="mb-4 text-lg">Authorization went Successful !!! <span className="font-semibold">{user.displayName}</span> ({user.email})</p>
-                    <button onClick={handleLogout} className="bg-blue-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                <div className="text-center">
+                    <p className="mb-4 text-black text-lg">Authorization went Successful !!! <span className="font-semibold">{user.displayName}</span> ({user.email})</p>
+                    <button onClick={handleLogout} className="bg-blue-400 hover:bg-purple-500 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         Logout
                     </button>
                     <div className="mt-4">
-                        <a href="/week-8/shopping-list" className="text-blue-400 hover:text-purple-500">Click here to Go to Shopping List </a>
+                        <a href="/week-8/shopping-list" className="text-black hover:text-purple-500">Click here to Go to Shopping List </a>
                     </div>
                 </div>
             </div>
@@ -31,8 +31,8 @@ function LandingPage() {
         return (
             <div className="flex justify-center items-center h-screen bg-blue-100">
                 <div className="text-center p-8 ">
-                    <button onClick={handleLogin} className="bg-blue-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Login with GitHub
+                    <button onClick={handleLogin} className="bg-blue-400 hover:bg-purple-500 text-white font-bold py-2 px-4 ">
+                        Login
                     </button>
                 </div>
             </div>
